36d4fc272f2140a235c7a21258f96d9e143ffe18
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-08 15:01:52 -0600

    Testing uniq

--- friends.rb
+++ friends.rb
@@ -1,4 +1,12 @@
-arrays = ["oliver", "taylor", "nick", "william", "grog"]
-puts arrays[2]
-puts arrays[0]
-puts arrays[4]
+# arrays = ["oliver", "taylor", "nick", "william", "grog"]
+# puts arrays[2]
+# puts arrays[0]
+# puts arrays[4]
+places = ["maine", "vermont", "arizona", "utah", "costa rica", "maine"]
+
+puts places.uniq
+puts "\n" 
+puts places
+
+places.uniq!
+puts places
